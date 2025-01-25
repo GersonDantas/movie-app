be51fdad3b4ed2904f75e394bb1b845587dc6875
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2025-01-25 11:34:03 -0300

    refactor(vite): simplify config and update alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ],
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
