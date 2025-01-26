16c442b3cf1795d7bdeb4990b73e7c577946aa43
gersondantas <53773325+GersonDantas@users.noreply.github.com>
2025-01-26 18:58:53 -0300

    feat(MovieCard): add fallback poster image and improve poster display

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import dayjs from 'dayjs'
 import type { Movie } from '@/types/movie'
 import CircularProgress from '@/components/CircularProgress.vue'
+
 const props = defineProps<{
   movie: Movie
 }>()
@@ -12,6 +13,13 @@ const score = computed(() => Math.round(props.movie.vote_average * 10))
 const formatDate = (date: string) => {
   return dayjs(date).format('MMM D, YYYY')
 }
+
+const posterUrl = computed(() => {
+  if (props.movie.poster_path) {
+    return `https://image.tmdb.org/t/p/w500${props.movie.poster_path}`
+  }
+  return 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'
+})
 </script>
 
 <template>
@@ -22,9 +30,9 @@ const formatDate = (date: string) => {
   >
     <div class="relative rounded-lg overflow-hidden mb-2">
       <img 
-        :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" 
+        :src="posterUrl" 
         :alt="movie.title"
-        class="w-full aspect-[2/3] object-cover"
+        class="w-full aspect-[2/3] object-cover bg-gray-800"
         data-testid="movie-poster"
       />
       <div class="absolute bottom-0 left-0 right-0 p-2 bg-gradient-to-t from-black/80">
